fc0ad0febfea5698e6454a9a861b0b961b49f8d8
musha <mushagh@gmail.com>
2017-11-01 19:20:04 +0800

    added example for member.js

---
+++ example_member.js
@@ -0,0 +1,14 @@
+// Lists all group member chat id hare
+
+// Place bot api here
+const botApi = 'Your API key';
+
+var users = [
+  // user id
+  73804315,
+  -214588509
+];
+
+// Export array
+module.exports.users = users;
+module.exports.botApi = botApi;
